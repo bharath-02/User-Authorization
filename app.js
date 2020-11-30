6d60e4e3b83bb3ba85635fe762cd7f965ff25857
bhara <bharathJ222000@gmail.com>
2020-11-30 13:06:29 +0530

    added

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const colors = require('colors');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 // view engine
 app.set('view engine', 'ejs');
 
-// database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
-
+// Connect to the database
+mongoose.connect('mongodb://localhost:27017/recipeDB', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err) => {
+    if (err) {
+        console.log('Error Occured'.red.bold);
+    } else {
+        console.log('Server connected to mongoDB'.cyan.bold);
+    }
+});
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
+
+// PORT
+app.listen(4040, () => {
+    console.log('Listening on PORT 4040'.yellow.bold);
+})
