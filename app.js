c8d86841c023372699aae3c313f5596b62191e54
bhara <bharathJ222000@gmail.com>
2020-12-05 16:21:37 +0530

    routes added

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = `mongodb+srv://Auth:bharath02@cluster0.vl7kg.mongodb.net/AuthDB?retryWrites=true&w=majority`;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err) => {
+const db = 'mongodb+srv://Auth:bharath02@cluster0.vl7kg.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const d = 'mongodb://localhost:27017/recipeDB';
+mongoose.connect(d, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err) => {
     if (err) {
         console.log('Error Occured while connecting to database');
     } else {
